a717581f5637917b835197051ee0af29fb9a5400
girinoboy@gmail.com <girinoboy@gmail.com@5ab4eb7e-5c73-0be5-10f2-145e72bf833e>
2013-10-10 05:49:31 +0000

    primeira versão loign

--- trunk/scdl/src/br/com/factory/HibernateUtility.java
+++ trunk/scdl/src/br/com/factory/HibernateUtility.java
@@ -91,7 +91,7 @@ public class HibernateUtility {
 //				.setProperty("hibernate.connection.datasource", "java:/GSWebDSOracle")//DS da pasta deploy Jboss
 				.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")// tipo de dialeto do banco
 				.setProperty("hibernate.connection.driver_class","com.mysql.jdbc.Driver")
-				.setProperty("hibernate.connection.datasource", "java:orenDS")
+				.setProperty("hibernate.connection.datasource", "java:scdlDS")
 				.setProperty("hibernate.hbm2ddl.auto", "update")
 				.setProperty("hibernate.c3p0.max_size", "10")
 				.setProperty("hibernate.c3p0.min_size", "2")
