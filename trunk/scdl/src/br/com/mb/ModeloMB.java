347ad3bddab069d9a3739690c81b4d289feb1b90
girinoboy@gmail.com <girinoboy@gmail.com@5ab4eb7e-5c73-0be5-10f2-145e72bf833e>
2013-10-21 02:42:39 +0000

    incluide funcional

--- trunk/scdl/src/br/com/mb/ModeloMB.java
+++ trunk/scdl/src/br/com/mb/ModeloMB.java
@@ -6,6 +6,6 @@ public interface ModeloMB {
 	
 	public void add(ActionEvent actionEvent) throws Exception;
 	public void edit(ActionEvent actionEvent) throws Exception;
-	public void del(ActionEvent actionEvent);
+	public void del(ActionEvent actionEvent) throws Exception;
 
 }
---
+++ trunk/scdl/src/br/com/utility/TurnoConverter.java
@@ -0,0 +1,72 @@
+package br.com.utility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+
+import br.com.dao.UsuarioDAO;
+import br.com.dto.TurnoDTO;
+import br.com.dto.UsuarioDTO;
+  
+@FacesConverter("turno")
+public class TurnoConverter implements Converter {  
+  
+//	private static UsuarioDAO usuarioDAO = new UsuarioDAO();
+    
+    public static List<TurnoDTO> turnoDB;  
+  
+    static {  
+        turnoDB = new ArrayList<TurnoDTO>();  
+        try {
+//			turnoDB = usuarioDAO.list();
+			
+			turnoDB = new ArrayList<TurnoDTO>();
+			turnoDB.add(new TurnoDTO(1,"Matutino"));
+			turnoDB.add(new TurnoDTO(2,"Vespertino"));
+			turnoDB.add(new TurnoDTO(3,"Noturno"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }  
+    
+  
+    public Object getAsObject(FacesContext facesContext, UIComponent component, String submittedValue) {
+    	
+        if (submittedValue == null || submittedValue.equals("null") || submittedValue.trim().equals("")) {
+            return null;
+        } else {
+            try {
+                int number = Integer.parseInt(submittedValue);
+                
+                for (TurnoDTO t : turnoDB) {
+                    if (t.getId() == number) {
+                        return t;
+                    }
+                }
+                
+                return submittedValue;
+  
+            } catch(NumberFormatException exception) {
+                throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Conversion Error", "Not a valid user")); 
+            } catch (Exception e) {
+            	throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Conversion Error", "Not a valid user"));
+			}
+        }
+  
+    }
+  
+    public String getAsString(FacesContext facesContext, UIComponent component, Object value) {  
+        if (value == null || value.equals("")) {  
+            return "";  
+        } else {  
+            return String.valueOf(((TurnoDTO) value).getId());  
+        }  
+    }  
+}  
