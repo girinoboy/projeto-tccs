4c4ea691cfea544cdb13518ee176d0f17fe6ec3b
girinoboy <girinoboy@5ab4eb7e-5c73-0be5-10f2-145e72bf833e>
2013-11-14 15:58:21 +0000

    novo caso de uso para o financeiro

---
+++ trunk/aam/src/br/com/dao/FinanceiroDAO.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package br.com.dao;
+
+import java.io.Serializable;
+
+import br.com.dto.FinanceiroDTO;
+
+/**
+ * @author marcleonio.medeiros
+ *
+ */
+public class FinanceiroDAO extends GenericoDAO<FinanceiroDTO, Serializable>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6593990035853478798L;
+
+	/**
+	 * 
+	 */
+	public FinanceiroDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
