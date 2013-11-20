7891a9842f95c77969347b5f0e3348c21530393d
girinoboy <girinoboy@5ab4eb7e-5c73-0be5-10f2-145e72bf833e>
2013-11-20 15:45:53 +0000

    relatorio de quantidade de escolas

--- scdl/src/br/com/dao/EscolaLivroDAO.java
+++ scdl/src/br/com/dao/EscolaLivroDAO.java
@@ -1,8 +1,13 @@
 package br.com.dao;
 
 import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
 
 import br.com.dto.EscolaLivroDTO;
+import br.com.factory.HibernateUtility;
 
 public class EscolaLivroDAO extends GenericoDAO<EscolaLivroDTO, Serializable>{
 
@@ -13,6 +18,13 @@ public class EscolaLivroDAO extends GenericoDAO<EscolaLivroDTO, Serializable>{
 
 	public EscolaLivroDAO() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public List<EscolaLivroDTO> consultaSQLQuery(String consulta) throws HibernateException, Exception {
+		Query query = HibernateUtility.getSession().createSQLQuery(consulta);
+
+
+				return query.list();
 	}
 
 }
