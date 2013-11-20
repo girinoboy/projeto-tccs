6aaa105cbec7ecc47e75815bef579b3a558adde0
mikaeltruth@gmail.com <mikaeltruth@gmail.com@5ab4eb7e-5c73-0be5-10f2-145e72bf833e>
2013-11-20 17:18:56 +0000

    Exclusão de escola !

--- trunk/scdl/src/br/com/dto/EscolaDTO.java
+++ trunk/scdl/src/br/com/dto/EscolaDTO.java
@@ -26,7 +26,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name="escola")
 public class EscolaDTO {
-	
+
 	@Id 
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Integer id;
@@ -42,46 +42,38 @@ public class EscolaDTO {
 	private Date data;
 	private Integer serie;
 	private String anotacoes;
-	private Boolean biblioteca;/*
-	@OneToMany(targetEntity=TurnoDTO.class, fetch = FetchType.LAZY, cascade= {CascadeType.ALL})
-	@JoinColumn(name = "escola_id", insertable = true, updatable = true, nullable = true)*/
-	/*
-	@ManyToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
-    @JoinTable(name = "IDS_SV_CHECK_HAS_TYPE",
-                joinColumns={@JoinColumn(name="check_id")},
-                inverseJoinColumns={@JoinColumn(name="checktype_id")})
-	*/
-	
-	 @ManyToMany  (cascade = CascadeType.ALL, fetch=FetchType.LAZY)
-	    @JoinTable(name = "escola_turno", joinColumns = { @JoinColumn(name = "escola_id",   
-	    unique = false, nullable = false, insertable = true) },   
-	    inverseJoinColumns = { @JoinColumn(name = "turno_id",   
-	    unique = false, nullable = false, insertable = true) })  
-	
+	private Boolean biblioteca;
+
+	@ManyToMany  (cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch=FetchType.LAZY)
+	@JoinTable(name = "escola_turno", joinColumns = { @JoinColumn(name = "escola_id",   
+	unique = false, nullable = false, insertable = true,updatable=true) },   
+	inverseJoinColumns = { @JoinColumn(name = "turno_id",   
+	unique = false, nullable = false, insertable = true,updatable=true) })  
+
 	private List<TurnoDTO> listTurnoDTO;
 
 	/**
 	 * 
 	 */
 	public EscolaDTO() {
-		
+
 	}
-	
+
 	@Override  
-    public boolean equals(Object obj) {  
-        if (obj == null) {  
-            return false;  
-        }  
-        if (getClass() != obj.getClass()) {  
-            return false;  
-        }  
-        final EscolaDTO other = (EscolaDTO) obj;
-        if (id != null && !this.id.equals(other.id)) {  
-            return false;
-        }  
-        return true;  
-    }
-	
+	public boolean equals(Object obj) {  
+		if (obj == null) {  
+			return false;  
+		}  
+		if (getClass() != obj.getClass()) {  
+			return false;  
+		}  
+		final EscolaDTO other = (EscolaDTO) obj;
+		if (id != null && !this.id.equals(other.id)) {  
+			return false;
+		}  
+		return true;  
+	}
+
 	public Integer getId() {
 		return id;
 	}
