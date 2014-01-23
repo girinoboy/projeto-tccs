f048b7979b1aab6447f77943222a6b5572487728
girinoboy <girinoboy@gmail.com>
2014-01-23 14:47:51 +0000

    correções gerais

--- aam/src/br/com/mb/FinanceiroMB.java
+++ aam/src/br/com/mb/FinanceiroMB.java
@@ -5,55 +5,107 @@ package br.com.mb;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+
+import org.hibernate.HibernateException;
+import org.primefaces.event.CellEditEvent;
 
 import br.com.dao.FinanceiroDAO;
 import br.com.dao.ParametroDAO;
+import br.com.dao.UsuarioDAO;
 import br.com.dto.FinanceiroDTO;
 import br.com.dto.ParametroDTO;
+import br.com.dto.UsuarioDTO;
 
 /**
  * @author marcleonio.medeiros
  *
  */
 @ManagedBean
+@RequestScoped
 public class FinanceiroMB extends GenericoMB implements ModeloMB{
-	
+
 	private FinanceiroDTO financeiroDTO = new FinanceiroDTO();
 	private FinanceiroDAO financeiroDAO = new FinanceiroDAO();
 	private List<FinanceiroDTO> listFinanceiroDTO;
+
+	private UsuarioDAO usuarioDAO = new UsuarioDAO();
 	/**
 	 * 
 	 */
 	public FinanceiroMB() {
 		try {
 			listFinanceiroDTO = financeiroDAO.list();
-			
-			ParametroDAO parametroDAO = new ParametroDAO();
-			ParametroDTO p = parametroDAO.recuperaParametro("mensalidade");
-			
-			financeiroDTO.setValorMensalidade(Double.valueOf(p.getValor()));
-			
-			
+
+			atualizaMensalidade();
+
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	private void atualizaMensalidade()throws Exception {
+		ParametroDAO parametroDAO = new ParametroDAO();
+		ParametroDTO p = parametroDAO.recuperaParametro("mensalidade");
+		if(financeiroDTO==null){
+			financeiroDTO = new FinanceiroDTO();
+			financeiroDTO.setValorMensalidade(Double.valueOf(p.getValor()));
+		}
+		else if(financeiroDTO.getValorMensalidade() != null && financeiroDTO.getValorMensalidade() != Double.valueOf(p.getValor())){
+			p.setValor(financeiroDTO.getValorMensalidade().toString());
+			parametroDAO.save(p);
+		}else{
+			financeiroDTO.setValorMensalidade(Double.valueOf(p.getValor()));
+		}
+
+	}
+
 	public void add(ActionEvent actionEvent) throws Exception {
-		financeiroDAO.save(financeiroDTO);
-		
+		atualizaMensalidade();
+		listFinanceiroDTO = financeiroDAO.list();
+		addMessage("Salvo.");
+		//financeiroDAO.save(financeiroDTO);
+
 	}
 
 	public void edit(ActionEvent actionEvent) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void del(ActionEvent actionEvent) throws Exception {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	public void onCellEdit(CellEditEvent event) throws Exception {
+		//		Object oldValue = event.getOldValue();
+		//		Object newValue = event.getNewValue();
+		UsuarioDTO usuarioDTO = listFinanceiroDTO.get(event.getRowIndex()).getUsuarioDTO();
+		usuarioDTO.getFinanceiroDTO().setValorComDesconto(calculaDesconto(usuarioDTO));
+		usuarioDAO.save(usuarioDTO);
+//		listFinanceiroDTO = financeiroDAO.list();
+		atualizaMensalidade();
+		//		if(newValue != null && !newValue.equals(oldValue)) {
+		//			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);  
+		//			FacesContext.getCurrentInstance().addMessage(null, msg);
+		//		}
+	}
+
+	public Double calculaDesconto(UsuarioDTO usuarioDTO) throws HibernateException, Exception{
+		ParametroDAO parametroDAO = new ParametroDAO();
+		ParametroDTO p = parametroDAO.recuperaParametro("mensalidade");
+		if(p != null && p.getValor() != null)
+			return (Double.valueOf(p.getValor()) - Double.valueOf(p.getValor()) * usuarioDTO.getDesconto());
+		else
+			return(0d);
 	}
 
 	public FinanceiroDTO getFinanceiroDTO() {
