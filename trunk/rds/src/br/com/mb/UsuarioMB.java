ce1e5b1d619800ad56f809257b5b2a32715df5a9
girinoboy@gmail.com <girinoboy@gmail.com@5ab4eb7e-5c73-0be5-10f2-145e72bf833e>
2014-05-10 04:59:20 +0000

    adicionado modeloMB

--- trunk/rds/src/br/com/mb/UsuarioMB.java
+++ trunk/rds/src/br/com/mb/UsuarioMB.java
@@ -5,10 +5,8 @@ package br.com.mb;
 
 import java.util.List;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 import org.primefaces.context.RequestContext;
@@ -39,6 +37,17 @@ public class UsuarioMB extends GenericoMB implements ModeloMB{
 			e.printStackTrace();
 		}
 	}
+	
+	@Override
+	public void atualiza(ActionEvent event) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void reset(ActionEvent event) { 
+        RequestContext.getCurrentInstance().reset("form:panel");  
+    } 
 	
 	
 	public void add(ActionEvent actionEvent) throws Exception {
@@ -80,17 +89,6 @@ public class UsuarioMB extends GenericoMB implements ModeloMB{
 		}
 	}
 	
-	public void reset() {  
-        RequestContext.getCurrentInstance().reset("form:panel");  
-    } 
-	//metodo generico que envia mesagens para a tela
-//	private void addMessage(FacesMessage message) {
-//		FacesContext.getCurrentInstance().addMessage(null, message);
-//	}
-	public void addMessage(String summary) {  
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);  
-		FacesContext.getCurrentInstance().addMessage(null, message);  
-	}
 
 	/**
 	 * @return the listUsuario
