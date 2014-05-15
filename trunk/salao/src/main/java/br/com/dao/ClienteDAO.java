71e84da4f05e001f82d31b513994502441ffaa93
joelson.save@gmail.com <joelson.save@gmail.com@5ab4eb7e-5c73-0be5-10f2-145e72bf833e>
2014-05-15 21:37:51 +0000

    atualização

--- trunk/salao/src/main/java/br/com/dao/ClienteDAO.java
+++ trunk/salao/src/main/java/br/com/dao/ClienteDAO.java
@@ -1,12 +1,6 @@
 package br.com.dao;
 
-import br.com.model.Cliente;
 
-public class ClienteDAO  extends DAO<Cliente> {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 3866458724829703777L;
+public class ClienteDAO  {
 
 }
--- trunk/salao/src/main/java/br/com/model/Usuario.java
+++ trunk/salao/src/main/java/br/com/model/Usuario.java
@@ -20,27 +20,23 @@ import javax.persistence.Table;
  * 
  */
 @Entity
-@Table(name = "usuario")
 public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = 250193575287542299L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue
 	private Long id;
-	
 	private String login;
 	private String senha;
-	private String nome;
-	private String telefone;
-	private String endereco;
-	private String email;
-	private String cpf;
-	private String observacao;
-	private String sexo;
-	
-	@Column(name = "data_nascimento")
-	private Date dataNascimento;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getLogin() {
 		return login;
@@ -56,91 +52,6 @@ public class Usuario implements Serializable {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+		
 	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-
-	public Date getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(Date dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getObservacao() {
-		return observacao;
-	}
-
-	public void setObservacao(String observacao) {
-		this.observacao = observacao;
-	}
-
-	public Integer getIdade() {
-
-		Calendar dateOfBirth = new GregorianCalendar();
-		if (dataNascimento != null) {
-			dateOfBirth.setTime(dataNascimento);
-		}
-		// Cria um objeto calendar com a data atual
-		Calendar today = Calendar.getInstance();
-
-		// Obtém a idade baseado no ano
-		int age = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR);
-
-		dateOfBirth.add(Calendar.YEAR, age);
-
-		if (today.before(dateOfBirth)) {
-			age--;
-		}
-		return age;
-
-	}
-
-	public String getSexo() {
-		return sexo;
-	}
-
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
-
 }
