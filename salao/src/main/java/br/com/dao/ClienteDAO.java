2ccbb25fb209d0ef6fb98acff8d9e78eb306a159
joelson.save <joelson.save@gmail.com>
2014-05-20 14:39:20 +0000

    atualização ayala

--- salao/src/main/java/br/com/dao/ClienteDAO.java
+++ salao/src/main/java/br/com/dao/ClienteDAO.java
@@ -1,6 +1,13 @@
 package br.com.dao;
 
+import br.com.model.Cliente;
 
-public class ClienteDAO  {
+
+public class ClienteDAO extends DAO<Cliente> {
+
+	public ClienteDAO(Class<Cliente> classe) {
+		super(classe);
+		// TODO Auto-generated constructor stub
+	}
 
 }
