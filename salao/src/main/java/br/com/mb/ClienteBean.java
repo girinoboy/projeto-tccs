76300fffacb3d0c5c43e967d70a38ca6247f513b
joelson.save <joelson.save@gmail.com>
2014-05-21 16:24:09 +0000

    Cadastro de cliente pronto 100%

--- salao/src/main/java/br/com/mb/ClienteBean.java
+++ salao/src/main/java/br/com/mb/ClienteBean.java
@@ -10,41 +10,41 @@ import br.com.model.Cliente;
 
 @RequestScoped
 @ManagedBean
-public class ClienteBean extends GenericBean{
+public class ClienteBean extends GenericBean {
 
 	private static final long serialVersionUID = 214643162316805969L;
-	
+
 	private Cliente cliente = new Cliente();
-	
+
 	private List<Cliente> clientes;
-		
+
 	public void grava() {
 		DAO<Cliente> dao = new DAO<Cliente>(Cliente.class);
-		
-		if (cliente.getId() == null) {			
+
+		if (cliente.getId() == null) {
 			dao.adicionar(cliente);
-		}else{
-			dao.atualiza(cliente);
+		} else {
+			dao.atualiza(getCliente());
 		}
 		addMessage("Salvo!");
 		clientes = dao.listaTodos();
 		this.cliente = new Cliente();
 	}
-	
-	public void remove(){
+
+	public void remove() {
 		DAO<Cliente> dao = new DAO<Cliente>(Cliente.class);
 		dao.remove(cliente);
 		addMessage("Removido");
 		this.clientes = dao.listaTodos();
 	}
-	
+
 	public List<Cliente> getClientes() {
 		if (clientes == null) {
 			clientes = new DAO<Cliente>(Cliente.class).listaTodos();
 		}
 		return clientes;
 	}
-	
+
 	public Cliente getCliente() {
 		return this.cliente;
 	}
