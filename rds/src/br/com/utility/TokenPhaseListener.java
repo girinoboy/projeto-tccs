58bfac880b787f7827ea3cb300666d2625ea35ae
girinoboy <girinoboy@gmail.com>
2014-11-17 17:11:00 +0000

    congelamento antes de ser criado banco de dados

--- rds/src/br/com/utility/TokenPhaseListener.java
+++
@@ -1,8 +0,0 @@
-package br.com.utility;
-
-
-public interface TokenPhaseListener extends javax.faces.event.PhaseListener {
-//	public void limpaCampos();
-
-
-}
--- scdl/src/br/com/utility/DataUtils.java
+++
@@ -1,5 +0,0 @@
-package br.com.utility;
-
-public class DataUtils {
-
-}
