23e2dfe4b66410be8f884c0faadc32e61544f031
girinoboy <girinoboy@gmail.com>
2014-11-19 03:41:44 +0000

    alerta de veiculo abastecendo demais pronto

--- controlefrota/src/main/java/br/com/dao/VeiculoDAO.java
+++ controlefrota/src/main/java/br/com/dao/VeiculoDAO.java
@@ -6,7 +6,12 @@ package br.com.dao;
 import java.io.Serializable;
 import java.util.List;
 
+import org.hibernate.Hibernate;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.transform.Transformers;
+import org.hibernate.type.LongType;
+import org.hibernate.type.StringType;
 
 import br.com.dto.VeiculoDTO;
 
@@ -37,6 +42,22 @@ public class VeiculoDAO extends GenericoDAO<VeiculoDTO, Serializable>{
 				.add(Restrictions.eqProperty("kmAtual", "kmLitro"))
 				.add(Restrictions.eqProperty("kmAtual", "kmRevisao")))
 				.list();
+		return list;
+	}
+
+	public List<VeiculoDTO> verificarAlertaGasto() {
+		List<VeiculoDTO> list ;//= criteria 
+//				.setProjection(Projections.projectionList().add( Projections.groupProperty("placa")
+//						).add(CustomProjections.groupByHaving(columns, aliases, types, "count(distinct id) >= 3 and name='1"
+//								+ "'")))
+//				.list();
+		
+		String sql = "select placa,km_atual as kmAtual ,km_litro as kmLitro from veiculo v"
+		+ " inner join abastecimento a on v.id = a.veiculo_id"
+		+ " group by placa"
+		+ " having km_atual < (km_litro * sum(quantidade_litros_abastecidos))";
+		list = session.createSQLQuery(sql).addScalar("placa",StringType.INSTANCE).addScalar("kmAtual",LongType.INSTANCE).addScalar("kmLitro",LongType.INSTANCE).setResultTransformer(Transformers  
+                .aliasToBean(VeiculoDTO.class)). list();
 		return list;
 	}
 
